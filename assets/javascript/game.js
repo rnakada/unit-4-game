2f1afa90746b5143700540f12b03a0d38ac4396d
ry <rnakada90@gmail.com>
2018-12-10 15:28:22 -0500

    12/10/2018

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,11 +1,7 @@
 $(document).ready(function () {
-// ----------------------------------------------------------------------------------------------------
-    // Global Variables
-// ----------------------------------------------------------------------------------------------------
-
-    var total = 0;
-    var wins = 0;
-    var losses = 0;
+// // ----------------------------------------------------------------------------------------------------
+//     // Global Variables
+// // ----------------------------------------------------------------------------------------------------
 
     var minNumber = parseFloat("19");
         // console.log(minNumber);
@@ -27,127 +23,87 @@ $(document).ready(function () {
     var crystalRandFour = Math.floor(Math.random() * crystalMaxNum) + crystalMinNum;
         console.log("crystal#4: " + crystalRandFour);
 
-// ----------------------------------------------------------------------------------------------------
-    
+    var userTotal = 0;
+    var wins = 0;
+    var losses = 0;
+// // ----------------------------------------------------------------------------------------------------
+    $("#wins").text(wins);
+    $("#losses").text(losses);
     $("#generateNumber").text(randNumber);
+// // ----------------------------------------------------------------------------------------------------
+    //Reset!
+    function reset(){
+        randNumber = Math.floor(Math.random() * maxNumber) + minNumber;
+        $("#generateNumber").text(randNumber);
+        crystalRandOne = Math.floor(Math.random() * crystalMaxNum) + crystalMinNum;
+        crystalRandTwo = Math.floor(Math.random() * crystalMaxNum) + crystalMinNum;
+        crystalRandThree = Math.floor(Math.random() * crystalMaxNum) + crystalMinNum;
+        crystalRandFour = Math.floor(Math.random() * crystalMaxNum) + crystalMinNum;
+        userTotal = 0;
+        $("playerScore").text(userTotal);
+    };
+// // ----------------------------------------------------------------------------------------------------
+    function winner() {
+        alert("Winner!")
+        wins++;
+        $("#wins").text(wins);
+        reset();
+    };
 
-// ----------------------------------------------------------------------------------------------------
-    $("#crystalOne, #crystalTwo, #crystalThree, #crystalFour").on("click", function() {
-        // alert("clicked");
-        if( crystalRandOne + total){
-            $("playerScore").text(total);
-            console.log(this);
+    function loser() {
+        alert("Loser!")
+        losses++;
+        $("#losses").text(losses);
+        reset();
+    };
+// // ----------------------------------------------------------------------------------------------------
+    // Crystal #1
+    $("#crystalOne").on("click", function() {
+        userTotal = userTotal + crystalRandOne;
+        console.log("userTotal: " + userTotal);
+        $("#playerScore").text(userTotal);
+        if (userTotal == randNumber) {
+            winner();
         }
-    })
-// ----------------------------------------------------------------------------------------------------
-
-
-    //  Generating Number @ start between 19-120
-    // ------------------------------------------------------------------------------------------------
-    // $(function mainNumber() {
-        // var minNumber = parseFloat("19");
-        // console.log(minNumber);
-        // var maxNumber = parseFloat("120");
-        // console.log(maxNumber);
-        // var randNumber = Math.floor(Math.random() * maxNumber) + minNumber;
-        // console.log("main#: " + randNumber);
-
-    //     $("#generateNumber").text(randNumber);
-    // });
-    // ------------------------------------------------------------------------------------------------
-
-    // Crystal images to generate a number between 1-12
-    // ------------------------------------------------------------------------------------------------
-    // $(function crystalOne() {
-
-        // var crystalRandOne = Math.floor(Math.random() * crystalMaxNum) + crystalMinNum;
-        // console.log("crystal#1: " + crystalRandOne);
-
-        // // var minNumber = parseFloat("1");
-        // // // console.log(minNumber);
-        // // var maxNumber = parseFloat("12");
-        // // // console.log(maxNumber);
-        // // var randNumber = Math.floor(Math.random() * maxNumber) + minNumber;
-        // console.log("crystal#1: " + randNumber);
-
-        // $("#crystalOne").on("click", function () {
-        //     $("#playerScore").append(randNumber);
-        // });
-
-        // $("#crystalOne").text(randNumber);
+        else if (userTotal > randNumber) {
+            loser();
+        }
     });
-    // ------------------------------------------------------------------------------------------------
-    $("#crystalOne, #crystalTwo, #crystalThree, #crystalFour").on("click", function() {
-
-    })
-    // ------------------------------------------------------------------------------------------------
-    // $(function crystalTwo() {
-
-        // var crystalRandTwo = Math.floor(Math.random() * crystalMaxNum) + crystalMinNum;
-        // console.log("crystal#2: " + crystalRandTwo);
-
-    //     // var minNumber = parseFloat("1");
-    //     // // console.log(minNumber);
-    //     // var maxNumber = parseFloat("12");
-    //     // // console.log(maxNumber);
-    //     // var randNumber = Math.floor(Math.random() * maxNumber) + minNumber;
-    //     // console.log("crystal#2: " + randNumber);
-
-    //     // $("#crystalTwo").on("click", function () {
-    //     //     $("#playerScore").append(randNumber);
-    //     // });
-
-    //     // // $("#crystalTwo").text(randNumber);
-    // })
-    // ------------------------------------------------------------------------------------------------
-    // $(function crystalThree() {
-
-        // var crystalRandThree = Math.floor(Math.random() * crystalMaxNum) + crystalMinNum;
-        // console.log("crystal#3: " + crystalRandThree);
-
-        // var minNumber = parseFloat("1");
-        // // console.log(minNumber);
-        // var maxNumber = parseFloat("12");
-        // // console.log(maxNumber);
-        // var randNumber = Math.floor(Math.random() * maxNumber) + minNumber;
-        // console.log("crystal#3: " + randNumber);
-
-        // $("#crystalThree").on("click", function () {
-        //     $("#playerScore").append(randNumber);
-        // });
-
-        // $("#crystalThree").text(randNumber);
-    // })
-    // ------------------------------------------------------------------------------------------------
-    // $(function crystalFour() {
-
-        // var crystalRandFour = Math.floor(Math.random() * crystalMaxNum) + crystalMinNum;
-        // console.log("crystal#4: " + crystalRandFour);
-
-        // var minNumber = parseFloat("1");
-        // // console.log(minNumber);
-        // var maxNumber = parseFloat("12");
-        // // console.log(maxNumber);
-        // var randNumber = Math.floor(Math.random() * maxNumber) + minNumber;
-        // console.log("crystal#4: " + randNumber);
-
-        // // $("#crystalFour").on("click", function () {
-        // //     $("#playerScore.total").append(randNumber);
-        // // });
-
-        // $("#crystalFour").on("click", function () {
-        //     $("#playerScore").append(randNumber);
-        // });
-
-        // $("#crystalFour").text(randNumber);
-    // })
-    // ------------------------------------------------------------------------------------------------
-    
-    // Player Score
-    // ------------------------------------------------------------------------------------------------
-    
-    // console.log("#playerScore");
-
-    // ------------------------------------------------------------------------------------------------
-
-// });
+    // Crystal #2
+    $("#crystalTwo").on("click", function() {
+        userTotal = userTotal + crystalRandTwo;
+        console.log("userTotal: " + userTotal);
+        $("#playerScore").text(userTotal);
+        if (userTotal == randNumber) {
+            winner();
+        }
+        else if (userTotal > randNumber) {
+            loser();
+        }
+    });
+    // Crystal #3
+    $("#crystalThree").on("click", function() {
+        userTotal = userTotal + crystalRandThree;
+        console.log("userTotal: " + userTotal);
+        $("#playerScore").text(userTotal);
+        if (userTotal == randNumber) {
+            winner();
+        }
+        else if (userTotal > randNumber) {
+            loser();
+        }
+    });
+    // Crystal #4
+    $("#crystalFour").on("click", function() {
+        userTotal = userTotal + crystalRandFour;
+        console.log("userTotal: " + userTotal);
+        $("#playerScore").text(userTotal);
+        if (userTotal == randNumber) {
+            winner();
+        }
+        else if (userTotal > randNumber) {
+            loser();
+        }
+    });
+});
+// ----------------------------------------------------------------------------------------------------
